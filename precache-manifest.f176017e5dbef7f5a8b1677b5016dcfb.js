6d2bd57cfe0e9c5403c83ffbdd59b42b7de32e65
DeveloperDowny <vedantpanchal12345@gmail.com>
2022-12-03 23:51:34 +0530

    Updates

---
+++ precache-manifest.f176017e5dbef7f5a8b1677b5016dcfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1783e8d0f0176fe8904a2cc0dfa3520d",
+    "url": "/multi-scenario-chess/index.html"
+  },
+  {
+    "revision": "3915ddfb0e015ac1bfd0",
+    "url": "/multi-scenario-chess/static/css/main.6c36a950.chunk.css"
+  },
+  {
+    "revision": "03beed76c670894df2e1",
+    "url": "/multi-scenario-chess/static/js/2.6f6d19a9.chunk.js"
+  },
+  {
+    "revision": "a3a7c184c9d4e2bd282372d19559c961",
+    "url": "/multi-scenario-chess/static/js/2.6f6d19a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3915ddfb0e015ac1bfd0",
+    "url": "/multi-scenario-chess/static/js/main.740c5e71.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/multi-scenario-chess/static/js/main.740c5e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd446b0539a16d05ce3d",
+    "url": "/multi-scenario-chess/static/js/runtime-main.3107cf04.js"
+  }
+]);
